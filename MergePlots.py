14b739d950e071f5ab00ab756ba39631cc613f7d
Tom <tom.f@claroty.com>
2020-12-20 16:45:13 +0200

    Created the MergePlot util

---
+++ MergePlots.py
@@ -0,0 +1,49 @@
+import fire
+import re
+import logging
+from pathlib import Path
+from PIL import Image
+
+logging.basicConfig(
+	format='%(asctime)s %(levelname)-8s %(message)s',
+	level=logging.INFO,
+	datefmt='%Y-%m-%d %H:%M:%S')
+
+
+NUMBER_REGEX = r"\d+"
+NUMBER_MATCHER = re.compile(NUMBER_REGEX)
+
+
+def merge_files(directory: Path, file_pattern: str, output: Path):
+	# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+	assert directory.is_dir()
+	relevant = list(directory.glob(file_pattern))
+	by_key = {}
+	for file in relevant:
+		integers = NUMBER_MATCHER.findall(file.name)
+		assert len(integers) == 1
+		key = integers[0]
+		by_key[key] = file
+
+	img, *imgs = [Image.open(f) for _, f in sorted(by_key.items())]
+	img.save(
+		fp=output, format='GIF', append_images=imgs, save_all=True, duration=200, loop=0)
+
+
+if __name__ == '__main__':
+	output_path = Path("~/university/jump_model_exp/")
+	visualized_path = Path("~/university/jump_model_exp/4096_island_out/visualized/").expanduser()
+	assert visualized_path.is_dir()
+	normalized_pattern = "normalized_*.png"
+	pattern = "island_*.png"
+
+	# Test
+	normalized = list(visualized_path.glob(normalized_pattern))
+	raw = list(visualized_path.glob(pattern))
+	assert len(normalized) == len(raw)
+	assert len(normalized) > 0
+	assert list(map(str, normalized)) != list(map(str, raw))
+	# Test
+
+	merge_files(visualized_path, pattern, output_path / "island-3-1024.gif")
+	merge_files(visualized_path, normalized_pattern, output_path / "norm-island-3-1024.gif")
