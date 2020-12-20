64c8b3e180c7c02961d6ec44edf7ebc41ec23f6d
Tom <tom.f@claroty.com>
2020-12-20 17:48:01 +0200

    Create the giff and compress it

--- MergePlots.py
+++ MergePlots.py
@@ -1,8 +1,13 @@
+from tempfile import TemporaryDirectory
+from typing import List
+import imageio
 import fire
 import re
 import logging
 from pathlib import Path
 from PIL import Image
+from pygifsicle import optimize as optimize_giff
+from src.time_func import time_func
 
 logging.basicConfig(
 	format='%(asctime)s %(levelname)-8s %(message)s',
@@ -19,19 +24,28 @@ def merge_files(directory: Path, file_pattern: str, output: Path):
 	assert directory.is_dir()
 	relevant = list(directory.glob(file_pattern))
 	by_key = {}
-	for file in relevant:
-		integers = NUMBER_MATCHER.findall(file.name)
-		assert len(integers) == 1
-		key = integers[0]
-		by_key[key] = file
-
-	img, *imgs = [Image.open(f) for _, f in sorted(by_key.items())]
-	img.save(
-		fp=output, format='GIF', append_images=imgs, save_all=True, duration=200, loop=0)
+	with time_func(f"Going over {len(relevant)} files"):
+		for file in relevant:
+			integers = NUMBER_MATCHER.findall(file.name)
+			assert len(integers) == 1
+			key = int(integers[0])
+			by_key[key] = file
+	steps = len(by_key) / 10
+	with time_func("Creating the GIFF"):
+		with imageio.get_writer(output, mode='I') as writer:
+			for index, (_, filename) in sorted(by_key.items()):
+				if index % steps == 0:
+					logging.info("Progress %s percent done", index / steps * 10)
+				image = imageio.imread(filename)
+				writer.append_data(image)
+	optimize_giff(output)
+	# img, *imgs = [Image.open(f) for _, f in sorted(by_key.items())]
+	# img.save(
+	# 	fp=output, format='GIF', append_images=imgs, save_all=True, duration=200, loop=0)
 
 
 if __name__ == '__main__':
-	output_path = Path("~/university/jump_model_exp/")
+	output_path = Path("~/university/jump_model_exp/").expanduser()
 	visualized_path = Path("~/university/jump_model_exp/4096_island_out/visualized/").expanduser()
 	assert visualized_path.is_dir()
 	normalized_pattern = "normalized_*.png"
@@ -45,5 +59,5 @@ if __name__ == '__main__':
 	assert list(map(str, normalized)) != list(map(str, raw))
 	# Test
 
-	merge_files(visualized_path, pattern, output_path / "island-3-1024.gif")
-	merge_files(visualized_path, normalized_pattern, output_path / "norm-island-3-1024.gif")
+	merge_files(visualized_path, pattern, output_path / "island-3-4096.gif")
+	merge_files(visualized_path, normalized_pattern, output_path / "norm-island-3-4096.gif")
