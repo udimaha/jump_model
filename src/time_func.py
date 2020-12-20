24b126b3637ef246bf3515d28141a92ba26e4900
Tom <tom.f@claroty.com>
2020-12-20 10:51:30 +0200

    Added a print at the beginning of time func for better logging

--- src/time_func.py
+++ src/time_func.py
@@ -5,6 +5,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def time_func(msg: str):
+    logging.info("Beginning to run %s", msg)
     time_before_call = time.monotonic()
     yield
     logging.info("%s took %s seconds", msg, round(time.monotonic() - time_before_call, 3))
