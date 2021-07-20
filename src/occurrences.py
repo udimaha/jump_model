80b23eb10801779c331b3bec9ae6d1a13eab3124
udimaha <36478958+udimaha@users.noreply.github.com>
2021-07-20 10:54:09 +0300

    Update occurrences.py
    Add meanOccs dict

--- src/occurrences.py
+++ src/occurrences.py
@@ -2,7 +2,7 @@ import struct
 from typing import Dict, List
 
 Occurrences = Dict[str, List[int]]
-
+meanOccs = Dict[str, float]
 
 def serialize_occurrences(to_serialize: Occurrences) -> bytes:
 	island_count = len(to_serialize)
